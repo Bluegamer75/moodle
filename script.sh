ecbd7c24b534e26690e12ced9fe7f809520a084e
bluegamer75 <76869585+Bluegamer75@users.noreply.github.com>
2021-11-10 13:52:33 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,14 +3,15 @@ apt-get install graphviz aspell ghostscript clamav git
 apt-get update
 apt-get install mysql-server mysql-client
 apt-get update
+apt install net-tools
 apt-get install apache2 php libapache2-mod-php php-cli php-mysql php-mbstring php-xmlrpc php-zip
 apt-get install php-gd php-xml php-bcmath php-ldap php-pspell  php-curl php-intl php-soap
 service apache2 restart
-mkdir /home/mikel/downloads
-cd /home/mikel/downloads
+mkdir downloads
+cd downloads
 wget https://download.moodle.org/stable38/moodle-latest-38.tgz
 tar -zxvf moodle-latest-38.tgz
-cp /home/mikel/downloads/moodle /var/www/html/ -R
+cp downloads/moodle /var/www/html/ -R
 chown www-data.www-data /var/www/html/moodle -R
 chmod 0755 /var/www/html/moodle -R
 mkdir /var/www/moodledata
