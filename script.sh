9180f9135e6d4d03bc3a5c3f26286aaac860be86
bluegamer75 <76869585+Bluegamer75@users.noreply.github.com>
2021-11-10 14:15:41 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 sudo su
-apt-get update
-apt-get install graphviz aspell ghostscript clamav git
-apt-get update
-apt-get install mysql-server mysql-client
-apt-get update
-apt install net-tools
-apt-get install apache2 php libapache2-mod-php php-cli php-mysql php-mbstring php-xmlrpc php-zip
-apt-get install php-gd php-xml php-bcmath php-ldap php-pspell  php-curl php-intl php-soap
+apt-get -y update
+apt-get -y install graphviz aspell ghostscript clamav git
+apt-get -yupdate
+apt-get -y install mysql-server mysql-client
+apt-get -y update
+apt install -y net-tools
+apt-get -y install apache2 php libapache2-mod-php php-cli php-mysql php-mbstring php-xmlrpc php-zip
+apt-get -y install php-gd php-xml php-bcmath php-ldap php-pspell  php-curl php-intl php-soap
 service apache2 restart
 mkdir downloads
 cd downloads
@@ -18,6 +18,6 @@ chmod 0755 /var/www/html/moodle -R
 mkdir /var/www/moodledata
 chown www-data /var/www/moodledata -R
 chmod 0770 /var/www/moodledata -R
+apt install -y gnome 
 mysql -u root -p
-apt install gnome 
 
