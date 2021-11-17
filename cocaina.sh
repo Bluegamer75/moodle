43a4eb6e02200c64fa344c92470951bd4ea2eaa1
bluegamer75 <76869585+Bluegamer75@users.noreply.github.com>
2021-11-17 13:37:47 +0100

    Update cocaina.sh

--- cocaina.sh
+++ cocaina.sh
@@ -3,11 +3,13 @@ apt-get install unzip
 unzip -q latest.zip -d /var/www/html/
 cd /var/www/html/wordpress
 chown www-data. -R /var/www/html/wordpress
-apt-get update
-apt-get install graphviz aspell ghostscript clamav git
-apt-get update
-apt-get install mysql-server mysql-client
-apt-get update
-apt-get install apache2 php libapache2-mod-php php-cli php-mysql php-mbstring php-xmlrpc php-zip
-apt-get install php-gd php-xml php-bcmath php-ldap php-pspell  php-curl php-intl php-soap
+apt-get -y update
+apt-get -yinstall graphviz aspell ghostscript clamav git
+apt-get -y update
+apt-get -y install mysql-server mysql-client
+apt-get -y update
+apt-get -y install apache2 php libapache2-mod-php php-cli php-mysql php-mbstring php-xmlrpc php-zip
+apt-get -y install php-gd php-xml php-bcmath php-ldap php-pspell  php-curl php-intl php-soap
+apt-get -y install net-tools
+apt-get -y install ssh
 mysql -u root -p
