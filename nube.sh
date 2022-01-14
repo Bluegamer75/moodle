adee1bdf212a8112631077a0810e85920d6726ec
bluegamer75 <76869585+Bluegamer75@users.noreply.github.com>
2022-01-14 09:38:10 +0100

    Update nube.sh

--- nube.sh
+++ nube.sh
@@ -1,20 +1,14 @@
-sudo apt update
-sudo apt install apache2
-sudo apt install mysql-server
-sudo mysql_secure_installation
-sudo mysql -u root -p
-sudo apt install php7.4
-sudo apt install php7.4-gd php7.4-mysql php7.4-curl php7.4-mbstring
-sudo apt install php7.4-intl php7.4-gmp php7.4-bcmath php7.4-xml
-sudo apt install php7.4-zip php-imagick php-apcu
-wget https://download.nextcloud.com/server/releases/nextcloud-21.0.0.zip
-sudo apt install unzip
-sudo unzip nextcloud-21.0.0.zip -d /var/www
-cd /var/www
-sudo chown -R www-data:www-data nextcloud/
-sudo a2enmod headers env dir mime
-cd /etc/apache2/sites-available/
-sudo nano 000-default.conf
-sudo service apache2 restart
-sudo apt install i3-wm
-startx
+apt upgrade
+apt install  ssh
+nano /etc/hostname
+nano /etc/hosts
+sudo systemctl disable systemd-resolved
+sudo systemctl stop systemd-resolved
+rm -f /etc/resolv.conf
+nano /etc/resolv.conf
+apt install dsnmasq
+systemctl restart dsnmasq
+wget https://files.zimbra.com/downloads/8.8.15_GA/zcs-8.8.15_GA_4179.UBUNTU20_64.20211118033954.tgz
+tar -xvf zcs-8.8.15_GA_4179.UBUNTU20_64.20211118033954.tgz
+cd zcs-8.8.15_GA_4179.UBUNTU20_64.20211118033954
+./install.sh
