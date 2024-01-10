1a1146ece220814b9b4e280a024c26393e26bdab
bluegamer75 <76869585+Bluegamer75@users.noreply.github.com>
2024-01-10 09:48:36 +0100

    Update nube.sh

--- nube.sh
+++ nube.sh
@@ -7,6 +7,7 @@ sudo systemctl stop systemd-resolved
 rm -f /etc/resolv.conf
 nano /etc/resolv.conf
 apt install dsnmasq
+nano /etc/dnsmasq.conf
 systemctl restart dsnmasq
 wget https://files.zimbra.com/downloads/8.8.15_GA/zcs-8.8.15_GA_4179.UBUNTU20_64.20211118033954.tgz
 tar -xvf zcs-8.8.15_GA_4179.UBUNTU20_64.20211118033954.tgz
